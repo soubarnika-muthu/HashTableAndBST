c081ae1754a03076f59b0239ace198b09dd9bb2c
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-11 21:27:39 +0530

    [Soubarnika]UC2-Finding Frequency of Large Paragraph using Hash Table Operation

--- HashTableAndBST/Program.cs
+++ HashTableAndBST/Program.cs
@@ -2,6 +2,8 @@
 
 namespace HashTableAndBST
 {
+    //UC1-Finding frequency of words using hash operation
+    //UC1-Finding frequency of large paragraph using hash operation
     class Program
     {
         static void Main(string[] args)
@@ -9,15 +11,30 @@ namespace HashTableAndBST
             Console.WriteLine("Implementaion of Hash Tables and Binary Search Tree");
             //creating object for MyMapnode clas
             MyMapNode<string, string> myMapNode = new MyMapNode<string, string>(6);
-            myMapNode.Add("1", "To");
-            myMapNode.Add("2", "be");
-            myMapNode.Add("3", "or");
-            myMapNode.Add("4", "not");
-            myMapNode.Add("5", "to");
-            myMapNode.Add("6", "be");
-            string key = "5";
+            
+            myMapNode.Add("1", "Paranoids");
+            myMapNode.Add("2", "are");
+            myMapNode.Add("3", "not");
+            myMapNode.Add("4", "paranoid");
+            myMapNode.Add("5", "because");
+            myMapNode.Add("6", "they");
+            myMapNode.Add("7", "are");
+            myMapNode.Add("8", "paranoid");
+            myMapNode.Add("9", "but");
+            myMapNode.Add("10", "because");
+            myMapNode.Add("11", "they");
+            myMapNode.Add("12", "keep");
+            myMapNode.Add("13", "putting");
+            myMapNode.Add("14", "themselves");
+            myMapNode.Add("15", "deliberately");
+            myMapNode.Add("16", "into ");
+            myMapNode.Add("17", "paranoid");
+            myMapNode.Add("18", "avoidable");
+            myMapNode.Add("19", "situations");
+            string key = "18";
             string value = myMapNode.Get(key);
             Console.WriteLine("key {0} holding value {1} ", key, value);
+
             Console.ReadLine();
         }
     }
